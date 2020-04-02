e7db85ee1c847d7b93c66776c27beab7b8d7417f
Kevin <691208yz@gmail.com>
2020-04-02 16:14:43 +0000

    add mongodb cloud

--- app.js
+++ app.js
@@ -9,13 +9,13 @@ var express = require('express'),
 	methodOverride = require('method-override'),
 	Comment = require('./models/comment.js'),
 	User = require('./models/user.js'),
-	seedDB = require('./seeds.js')
+	seedDB = require('./seeds.js');
 
 var commentRoutes = require('./routes/comments.js'),
 	campgroundRoutes = require('./routes/campgrounds.js'),
 	authRoutes = require('./routes/index');
 
-mongoose.connect('mongodb://localhost/camp', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://Kevi:q691208@cluster0-3xnnp.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 app.use(methodOverride('_method'));
